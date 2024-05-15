453644ea4932ec2f6ba2a726565b4ae51b217e17
Oussema Dallel <oussemadallel@gmail.com>
2024-05-15 16:47:24 +0200

    feat: add ModelViewer

---
+++ src/features/common/ModelViewer/components/Viewer.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from '@react-three/fiber';
+import { Model } from './Model';
+import { type FunctionComponent, type ReactElement, Suspense } from 'react';
+import { Html, OrbitControls } from '@react-three/drei';
+
+interface ViewerProps {
+	readonly modelSrc: string;
+}
+
+const Viewer: FunctionComponent<ViewerProps> = ({ modelSrc }): ReactElement => {
+	return (
+		<Canvas>
+			<ambientLight intensity={ 0.2 } />
+			<directionalLight
+				color="red"
+				position={ [ 0, 0, 5 ] }
+			/>
+			<Suspense
+				fallback={
+					<Html>
+						<h1>loading ...</h1>
+					</Html>
+				}
+			>
+				<Model
+					src={ modelSrc }
+				/>
+			</Suspense>
+			<OrbitControls
+				autoRotate
+				enableDamping
+			/>
+		</Canvas>
+	);
+};
+
+/* eslint-enable react/no-unknown-property */
+
+export { Viewer };
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,17 @@
 import type { Asset } from '../features/modules/AssestsManager/types/Asset';
 import sneakerBlue from '../features/modules/AssestsManager/assets/sneaker_blue.png';
+// @ts-expect-error this should work fine as the config is updated to include splat files.
+import sneakerBlue3d from '../features/common/ModelViewer/assets/sneaker_blue.splat';
 import sneakerBlueSecond from '../features/modules/AssestsManager/assets/sneaker_second_blue.png';
 import sneakerBlueThird from '../features/modules/AssestsManager/assets/sneaker_third_blue.png';
 import sneakerColored from '../features/modules/AssestsManager/assets/sneaker_colored.png';
+// @ts-expect-error this should work fine as the config is updated to include splat files.
+import sneakerColored3d from '../features/common/ModelViewer/assets/sneaker_colored.splat';
 import sneakerColoredSecond from '../features/modules/AssestsManager/assets/sneaker_second_colored.png';
 import sneakerColoredThird from '../features/modules/AssestsManager/assets/sneaker_third_colored.png';
 import sneakerWhite from '../features/modules/AssestsManager/assets/sneaker_white.png';
+// @ts-expect-error this should work fine as the config is updated to include splat files.
+import sneakerWhite3d from '../features/common/ModelViewer/assets/sneaker_white.splat';
 import sneakerWhiteSecond from '../features/modules/AssestsManager/assets/sneaker_second_white.jpg';
 import sneakerWhiteThird from '../features/modules/AssestsManager/assets/sneaker_third_white.jpeg';
 import type { DefaultBodyType, HttpHandler, PathParams } from 'msw';
@@ -16,7 +22,8 @@ const data = {
 		{
 			/* eslint-disable @typescript-eslint/naming-convention */
 			'2d_outputs': [ sneakerBlue, sneakerBlueSecond, sneakerBlueThird ],
-			'3d_output': '3d_output_1',
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			'3d_output': sneakerBlue3d,
 			description: 'description',
 			assetId: '1',
 			inputs: [ sneakerBlue, sneakerBlueSecond, sneakerBlueThird ],
@@ -25,7 +32,8 @@ const data = {
 		},
 		{
 			'2d_outputs': [ sneakerWhite, sneakerWhiteSecond, sneakerWhiteThird ],
-			'3d_output': '3d_output_1',
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			'3d_output': sneakerWhite3d,
 			description: 'description',
 			assetId: '2',
 			inputs: [ sneakerWhite, sneakerWhiteSecond, sneakerWhiteThird ],
@@ -34,7 +42,8 @@ const data = {
 		},
 		{
 			'2d_outputs': [ sneakerWhite, sneakerWhiteSecond, sneakerWhiteThird ],
-			'3d_output': [ '3d_output_1', '3d_output_2' ],
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			'3d_output': sneakerWhite3d,
 			description: 'description',
 			assetId: '3',
 			inputs: [ sneakerWhite, sneakerWhiteSecond, sneakerWhiteThird ],
@@ -43,7 +52,8 @@ const data = {
 		},
 		{
 			'2d_outputs': [ sneakerColored, sneakerColoredSecond, sneakerColoredThird ],
-			'3d_output': [ '3d_output_1', '3d_output_2' ],
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			'3d_output': sneakerColored3d,
 			description: 'description',
 			assetId: '4',
 			inputs: [ sneakerColored, sneakerColoredSecond, sneakerColoredThird ],
